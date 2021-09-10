2a775184bc14febd6de39a1040d5fe2b0ad2cef9
00MOREIRA00 <85044936+00MOREIRA00@users.noreply.github.com>
2021-09-09 22:07:22 -0300

    Add files via upload

---
+++ Projeto Miguel/chatbot-certo/constants.js
@@ -0,0 +1,66 @@
+// Options the user could type in
+const prompts = [
+  ["oi", "ola", "iai", "bom dia", "boa tarde"],
+  ["como voce esta", "como esta", "como vai"],
+  ["preciso de ajuda", "tem como me ajudar", "ajuda"],
+  ["preciso de uma peticao", "quero criar uma peticao", "problema judicial"],
+  ["peticao para vara de familia", "peticao para empresa de energia", "peticao para empresa de agua", "peticao para fins trabalhistas"],
+  ["ta bom", "ok"],
+  [
+    "nao, obrigado",
+    "nao",
+    "so isso",
+  ],
+  ["tchau"],
+  ["conta uma piada ai",],
+  ["se despede da galera"],
+  ["help me", "tell me story", "tell me joke"],
+  ["ah", "yes", "ok", "okay", "nice"],
+  ["bye", "good bye", "goodbye", "see you later"],
+  ["what should i eat today"],
+  ["bro"],
+  ["what", "why", "how", "where", "when"],
+  ["no","not sure","maybe","no thanks"],
+  [""],
+  ["haha","ha","lol","hehe","funny","joke"]
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+  ["ola! eu sou Miguel e sou seu assistente virtual"],
+  [
+    "estou bem e feliz"
+  ],
+  [
+    "como posso ajudar?"
+  ],
+  ["vou tentar ajudar o maximo que posso, vamos ver. Que tipo de peticao voce precisa?"],
+  ["Ok, O download sera iniciado no fim do atendimento"],
+  ["Posso ajudar em algo mais?"],
+  ["Agradecemos o acesso, tenha um bom dia e nao esqueca de seguir o rolezinho de t i",],
+  ["tchau",],
+  ["eu nao sei, mas o professor J P conta umas maneiras"],
+  ["Galera, obrigado pela atencao. Espero que tenham gostado e por favor sigam o rolezinho de T I"],
+  ["What about?", "Once upon a time..."],
+  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+  ["Bye", "Goodbye", "See you later"],
+  ["Sushi", "Pizza"],
+  ["Bro!"],
+  ["Great question"],
+  ["That's ok","I understand","What do you want to talk about?"],
+  ["Please say something :("],
+  ["Haha!","Good one!"]
+]
+
+// Random for any other user input
+
+const alternative = [
+  "Que?",
+  "Brother, to entendendo legal nao sksksks",
+  
+]
+
+// Whatever else you want :)
+
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
